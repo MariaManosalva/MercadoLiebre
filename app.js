c470e9e1fc23e4231c6958370a8b756b11f29092
Maria Manosalva <cate0196@hotmail.com>
2022-05-30 17:48:30 -0500

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require ('express');
 const path = require ('path');
-
 const app = express();
 
 app.use ( express.static( path.resolve ( __dirname , 'public')));
@@ -8,5 +7,13 @@ app.use ( express.static( path.resolve ( __dirname , 'public')));
 app.listen (3000 , () => console.log ('Servidor levantado'));
 
 app.get ('/' , ( req , res ) =>{
-    res.sendFile ( path.resolve (__dirname , 'views/index.html'));
+    res.sendFile ( path.resolve (__dirname , 'views/home.html'));
+})
+
+app.get ('/login' , ( req , res ) =>{
+    res.sendFile ( path.resolve (__dirname , 'views/login.html'));
+})
+
+app.get ('/register' , ( req , res ) =>{
+    res.sendFile ( path.resolve (__dirname , 'views/register.html'));
 })
